164748caef96c39eac29d0ae2b4897e037532136
Luiz Filipe <luizvasc9@gmail.com>
2022-03-14 16:56:27 -0300

    v2

--- lib/pages/boasvindas_page/boasvindas_page.dart
+++ lib/pages/boasvindas_page/boasvindas_page.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
               ),
             ),
           ),
+          Container(),
           Positioned(
             top: 450,
             left: 150,
--- lib/widgets/sesiFitness_form.dart
+++ lib/widgets/sesiFitness_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SesifitnessForm extends StatelessWidget {
   const SesifitnessForm({Key? key}) : super(key: key);
@@ -10,6 +11,9 @@ class SesifitnessForm extends StatelessWidget {
         borderRadius: BorderRadius.circular(30),
         color: const Color(0x00ffffff),
       ),
+      width: context.widthTransformer(reducedBy: 60),
+      height: 50,
+      alignment: Alignment.center,
     );
   }
 }
