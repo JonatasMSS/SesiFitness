fd4f289f231f6c8d101893ff27edf25cf43072c5
Luiz Filipe <luizvasc9@gmail.com>
2022-03-15 15:55:17 -0300

    Adição de comemtarios e modificação da appbar

--- lib/widgets/sesiFitness_appbar.dart
+++ lib/widgets/sesiFitness_appbar.dart
@@ -1,14 +1,20 @@
+// ignore: file_names
 import 'package:flutter/material.dart';
+// Criando a APPBAR personalizada
 
 class SesifitnessAppbar extends AppBar {
   SesifitnessAppbar({
     Key? key,
     double elevation = 2,
   }) : super(
-            key: key,
-            backgroundColor: Color(0xFF274776),
-            elevation: elevation,
-            centerTitle: true,
-            title: const Text("SESI FITNESS"),
-            iconTheme: const IconThemeData(color: Colors.black));
+          //Definindo os parametros e cores
+          key: key,
+          backgroundColor: Color(0xFF274776),
+          elevation: elevation,
+          centerTitle: true,
+          title: const Text("SESI FITNESS"),
+          iconTheme: const IconThemeData(
+            color: Colors.black,
+          ),
+        );
 }
