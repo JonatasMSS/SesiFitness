e3e72c55f892e1a41902ed2373c96ab1d7489329
Luiz Filipe <luizvasc9@gmail.com>
2022-03-16 14:04:27 -0300

    Adicionando o const

--- lib/widgets/sesiFitness_button.dart
+++ lib/widgets/sesiFitness_button.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class SesifitnessButton extends StatelessWidget {
-  SesifitnessButton({
+  const SesifitnessButton({
     Key? key,
     this.textDesc = "TEXTO",
   }) : super(key: key);
@@ -16,20 +16,20 @@ class SesifitnessButton extends StatelessWidget {
         Get.snackbar(
           'Error',
           "Usuário não encontrado",
-          colorText: Color(0xFFFFFFFF),
-          backgroundColor: Color(0xFFB41212),
+          colorText: const Color(0xFFFFFFFF),
+          backgroundColor: const Color(0xFFB41212),
         );
       },
       style: ElevatedButton.styleFrom(
-        shape: StadiumBorder(),
-        primary: Color(0xFF0DFFA6),
+        shape: const StadiumBorder(),
+        primary: const Color(0xFF0DFFA6),
       ),
       child: Container(
         child: Text(
           textDesc,
-          style: TextStyle(color: Color(0xFF005A6F)),
+          style: const TextStyle(color: Color(0xFF005A6F)),
         ),
-        margin: EdgeInsets.all(10),
+        margin: const EdgeInsets.all(10),
       ),
     );
   }
