7c93f55be29bdb174c0111f3732422e46bd9d115
JonatasMSS <jonatas.miguelss@gmail.com>
2022-04-01 16:45:17 -0300

    Implementação de novas funcionalidades --> Conexão ao banco(Melhorada) | Abstração de código

--- lib/pages/meusTreinos_page/meusTreinos/meusTreinos_controller.dart
+++ lib/pages/meusTreinos_page/meusTreinos/meusTreinos_controller.dart
@@ -6,7 +6,7 @@ import 'package:sesi_fitness/models/dayModel/dayModel.dart';
 import 'package:sesi_fitness/pages/avaliacoesaAluno_page/avaliacoesaAluno_page_page.dart';
 import 'package:sesi_fitness/repository/userAuth_repository/i_userAuth.dart';
 
-class MeusTreinosController extends GetxController {
+class MeusTreinosController extends GetxController with StateMixin {
   RxBool changeContainer = false.obs;
   final AlunoModel alunoData = Get.arguments;
   final IUserAuth _iUserAuth;
@@ -17,6 +17,7 @@ class MeusTreinosController extends GetxController {
   void onInit() {
     // TODO: implement onInit
     findTreinosById(alunoData.id);
+
     super.onInit();
   }
 
--- lib/pages/meusTreinos_page/meusTreinos/meusTreinos_page.dart
+++ lib/pages/meusTreinos_page/meusTreinos/meusTreinos_page.dart
@@ -1,15 +1,29 @@
-import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'package:sesi_fitness/models/dayModel/dayModel.dart';
+import 'package:sesi_fitness/models/treinosModel/treinosModel.dart';
 import 'package:sesi_fitness/widgets/sesiAcadeima_listaTreinos.dart';
 import 'package:sesi_fitness/widgets/sesiAcademia.appbarButtons.dart';
 import 'package:sesi_fitness/widgets/sesiAcademia_pageDrawer.dart';
 import 'package:sesi_fitness/widgets/sesiAcademia_treino.dart';
-
 import 'package:sesi_fitness/widgets/sesiFitness_appbar.dart';
+
 import './meusTreinos_controller.dart';
 
 class MeusTreinosPage extends GetView<MeusTreinosController> {
   MeusTreinosPage({Key? key}) : super(key: key);
+
+  DayModel returnTrainByDay(String name) {
+    final dataGet = controller.treinosAluno;
+    dynamic data;
+    for (var i = 0; i < dataGet.length; i++) {
+      if (dataGet[i].diaSemana == name) {
+        data = dataGet[i];
+      }
+    }
+    return data ?? DayModel(id: "", alunoId: "", diaSemana: "", treinosDia: []);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,119 +31,147 @@ class MeusTreinosPage extends GetView<MeusTreinosController> {
       drawer: pageDrawer(person: controller.alunoData),
       backgroundColor: Color(0xFFEFEFEF),
       appBar: SesifitnessAppbar(),
-      body: SingleChildScrollView(
-        child: Container(
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: SesiacademiaAppbarbuttons(
-                      sizeH: 50,
-                      descText: "Meus Treinos",
-                      state: true,
-                      textColor: Colors.black,
+      body: FutureBuilder(
+        future: controller.findTreinosById(controller.alunoData.id),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return SingleChildScrollView(
+              child: Container(
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: SesiacademiaAppbarbuttons(
+                            sizeH: 50,
+                            descText: "Meus Treinos",
+                            state: true,
+                            textColor: Colors.black,
+                          ),
+                        ),
+                        Expanded(
+                          child: SesiacademiaAppbarbuttons(
+                            sizeH: 50,
+                            descText: "Minhas Avaliações",
+                            state: false,
+                            pageRoute: '/minhasAvaliacoes',
+                            arg: controller.alunoData,
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                  Expanded(
-                    child: SesiacademiaAppbarbuttons(
-                      sizeH: 50,
-                      descText: "Minhas Avaliações",
-                      state: false,
-                      pageRoute: '/minhasAvaliacoes',
-                      arg: controller.alunoData,
+                    const SizedBox(
+                      height: 15,
                     ),
-                  ),
-                ],
+                    SesiacadeimaListatreinos(
+                      titleList: "Segunda",
+                      containList: [
+                        DataListTreino(
+                            BdataTreino: returnTrainByDay("segunda")),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    SesiacadeimaListatreinos(
+                      titleList: "Terça",
+                      containList: [
+                        DataListTreino(BdataTreino: returnTrainByDay("terca"))
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    SesiacadeimaListatreinos(
+                      titleList: "Quarta",
+                      containList: [
+                        SesiacademiaTreino(
+                          assetImage: 'assets/images/treino.png',
+                          pageRoute: '/treinoDetalhado',
+                          title: "Treino A",
+                          repetition: "3x15 repetições",
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    SesiacadeimaListatreinos(
+                      titleList: "Quinta",
+                      containList: [
+                        SesiacademiaTreino(
+                          assetImage: 'assets/images/treino.png',
+                          pageRoute: '/treinoDetalhado',
+                          title: "Treino A",
+                          repetition: "3x15 repetições",
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    SesiacadeimaListatreinos(
+                      titleList: "Sexta",
+                      containList: [
+                        SesiacademiaTreino(
+                          assetImage: 'assets/images/treino.png',
+                          pageRoute: '/treinoDetalhado',
+                          title: "Treino A",
+                          repetition: "3x15 repetições",
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    SesiacadeimaListatreinos(
+                      titleList: "Sábado",
+                      containList: [
+                        SesiacademiaTreino(
+                          assetImage: 'assets/images/treino.png',
+                          pageRoute: '/treinoDetalhado',
+                          title: "Treino A",
+                          repetition: "3x15 repetições",
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-              const SizedBox(
-                height: 15,
-              ),
-              SesiacadeimaListatreinos(
-                titleList: "Segunda",
-                containList: [
-                  SesiacademiaTreino(
-                    assetImage: 'assets/images/treino.png',
-                    pageRoute: '/treinoDetalhado',
-                    title: "Treino de Perna",
-                    repetition: "3x15 repetições",
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              SesiacadeimaListatreinos(
-                titleList: "Terça",
-                containList: [
-                  SesiacademiaTreino(
-                    assetImage: 'assets/images/treino.png',
-                    pageRoute: '/treinoDetalhado',
-                    title: "Treino A",
-                    repetition: "3x15 repetições",
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              SesiacadeimaListatreinos(
-                titleList: "Quarta",
-                containList: [
-                  SesiacademiaTreino(
-                    assetImage: 'assets/images/treino.png',
-                    pageRoute: '/treinoDetalhado',
-                    title: "Treino A",
-                    repetition: "3x15 repetições",
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              SesiacadeimaListatreinos(
-                titleList: "Quinta",
-                containList: [
-                  SesiacademiaTreino(
-                    assetImage: 'assets/images/treino.png',
-                    pageRoute: '/treinoDetalhado',
-                    title: "Treino A",
-                    repetition: "3x15 repetições",
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              SesiacadeimaListatreinos(
-                titleList: "Sexta",
-                containList: [
-                  SesiacademiaTreino(
-                    assetImage: 'assets/images/treino.png',
-                    pageRoute: '/treinoDetalhado',
-                    title: "Treino A",
-                    repetition: "3x15 repetições",
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              SesiacadeimaListatreinos(
-                titleList: "Sábado",
-                containList: [
-                  SesiacademiaTreino(
-                    assetImage: 'assets/images/treino.png',
-                    pageRoute: '/treinoDetalhado',
-                    title: "Treino A",
-                    repetition: "3x15 repetições",
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
     );
+  }
+}
+
+class DataListTreino extends StatelessWidget {
+  const DataListTreino({
+    Key? key,
+    required this.BdataTreino,
+  }) : super(key: key);
+
+  final DayModel BdataTreino;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        itemCount: BdataTreino.treinosDia.length,
+        itemBuilder: (_, index) {
+          final treino = BdataTreino.treinosDia[index];
+          return SesiacademiaTreino(
+            assetImage: 'assets/images/treino.png',
+            pageRoute: '/treinoDetalhado',
+            title: treino.tituloTreino,
+            repetition: treino.repTreino,
+          );
+        });
   }
 }
