87ef8b07cfbcbd65e42d376960d60be1330bc05c
JonatasMSS <jonatas.miguelss@gmail.com>
2022-04-06 14:21:50 -0300

    Adição de FIREBASE no código

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 //Criado por Jonatas Miguel e Luiz Felipe
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sesi_fitness/pages/Theme/academiaSesi.dart';
@@ -24,7 +25,10 @@ import 'package:sesi_fitness/pages/rodrigoPage_page/rodrigoPage_page_page.dart';
 import 'package:sesi_fitness/pages/treinoDetalhado_page/treinoDetalhado_bindings.dart';
 import 'package:sesi_fitness/pages/treinoDetalhado_page/treinoDetalhado_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(sesiFitnessMAIN());
 }
 
